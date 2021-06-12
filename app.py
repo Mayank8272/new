5a3104b4e1d836a9fc04174d641686b7e91c2d30
Mayank8272 <85300292+Mayank8272@users.noreply.github.com>
2021-06-13 04:41:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    app.route('/')
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
